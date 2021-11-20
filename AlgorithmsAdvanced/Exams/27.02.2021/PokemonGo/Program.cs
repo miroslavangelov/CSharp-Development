e2f65b163fdbb5ff191cd5fc95cd67724f925c2a
miroslav_angelov_latona <miroslav.angelov@latona.eu>
2021-11-20 13:56:23 +0200

    add advanced algorithms exam

---
+++ AlgorithmsAdvanced/Exams/27.02.2021/PokemonGo/Program.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PokemonGo
+{
+    class Program
+    {
+        private static List<Street> streets;
+        private static int[,] knapSack;
+
+        static void Main(string[] args)
+        {
+            int fuel = int.Parse(Console.ReadLine());
+            string nextRow = Console.ReadLine();
+            streets = new List<Street>();
+
+            while (nextRow != "End")
+            {
+                string[] streetData = nextRow.Split(", ", StringSplitOptions.RemoveEmptyEntries);
+                string streetName = streetData[0];
+                int pokemonsCount = int.Parse(streetData[1]);
+                int streetLength = int.Parse(streetData[2]);
+                Street street = new Street
+                {
+                    Name = streetName,
+                    Length = streetLength,
+                    Value = pokemonsCount
+                };
+                streets.Add(street);
+
+                nextRow = Console.ReadLine();
+            }
+
+            knapSack = new int[streets.Count + 1, fuel + 1];
+            fillKnapsack();
+
+            SortedSet<string> selectedStreets = new SortedSet<string>();
+            int row = knapSack.GetLength(0) - 1;
+            int col = knapSack.GetLength(1) - 1;
+            int usedFuel = 0;
+            int totalValue = 0;
+
+            while (row > 0 && col > 0)
+            {
+                if (knapSack[row, col] != knapSack[row - 1, col])
+                {
+                    Street selectedItem = streets[row - 1];
+
+                    selectedStreets.Add(selectedItem.Name);
+                    usedFuel += selectedItem.Length;
+                    totalValue += selectedItem.Value;
+                    col -= selectedItem.Length;
+                }
+
+                row -= 1;
+            }
+
+            if (selectedStreets.Count > 0)
+            {
+                Console.WriteLine(string.Join(" -> ", selectedStreets));
+            }
+            Console.WriteLine($"Total Pokemon caught -> {totalValue}");
+            Console.WriteLine($"Fuel Left -> {fuel - usedFuel}");
+        }
+
+        public class Street
+        {
+            public string Name { get; set; }
+
+            public int Value { get; set; }
+
+            public int Length { get; set; }
+
+            public override string ToString()
+            {
+                return Name;
+            }
+        }
+
+        private static void fillKnapsack() {
+            for (int itemIndex = 1; itemIndex < knapSack.GetLength(0); itemIndex++)
+            {
+                var currentItem = streets[itemIndex - 1];
+
+                for (int capacity = 1; capacity < knapSack.GetLength(1); capacity++)
+                {
+                    if (capacity < currentItem.Length)
+                    {
+                        knapSack[itemIndex, capacity] = knapSack[itemIndex - 1, capacity];
+                    }
+                    else
+                    {
+                        knapSack[itemIndex, capacity] = Math.Max(
+                            knapSack[itemIndex - 1, capacity],
+                            knapSack[itemIndex - 1, capacity - currentItem.Length] + currentItem.Value);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ AlgorithmsAdvanced/Exams/27.02.2021/Robbery/Program.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Wintellect.PowerCollections;
+
+namespace Robbery
+{
+    class Program
+    {
+        private static List<Edge>[] graph;
+        private static bool[] cameras;
+
+        static void Main(string[] args)
+        {
+            int nodesCount = int.Parse(Console.ReadLine());
+            int edgesCount = int.Parse(Console.ReadLine());
+            graph = ReadGraph(nodesCount, edgesCount);
+            cameras = ReadCameras(nodesCount);
+            int startNode = int.Parse(Console.ReadLine());
+            int endNode = int.Parse(Console.ReadLine());
+            double[] distances = new double[nodesCount];
+
+            for (int i = 0; i < nodesCount; i++)
+            {
+                distances[i] = double.PositiveInfinity;
+            }
+
+            var queue = new OrderedBag<int>(Comparer<int>.Create((f, s) => distances[f].CompareTo(distances[s])));
+            distances[startNode] = 0;
+            queue.Add(startNode);
+
+            while (queue.Count > 0)
+            {
+                var node = queue.RemoveFirst();
+
+                if (node == endNode)
+                {
+                    break;
+                }
+
+                foreach (var edge in graph[node])
+                {
+                    var child = edge.First == node ? edge.Second : edge.First;
+
+                    if (cameras[child])
+                    {
+                        continue;
+                    }
+
+                    if (double.IsPositiveInfinity(distances[child]))
+                    {
+                        queue.Add(child);
+                    }
+
+                    var newDistance = distances[node] + edge.Weight;
+
+                    if (newDistance < distances[child])
+                    {
+                        distances[child] = newDistance;
+                        queue = new OrderedBag<int>(queue, Comparer<int>.Create((f, s) => distances[f].CompareTo(distances[s])));
+                    }
+                }
+            }
+
+            Console.WriteLine(distances[endNode]);
+        }
+
+        private static bool[] ReadCameras(int nodesCount)
+        {
+            bool[] result = new bool[nodesCount];
+            string[] camerasData = Console.ReadLine().Split();
+
+            for (int node = 0; node < camerasData.Length; node++)
+            {
+                char blackOrWhite = camerasData[node][1];
+
+                if (blackOrWhite == 'b')
+                {
+                    result[node] = false;
+                }
+                else
+                {
+                    result[node] = true;
+                }
+            }
+
+            return result;
+        }
+        
+        private static List<Edge>[] ReadGraph(int nodesCount, int edgesCount)
+        {
+            List<Edge>[] result = new List<Edge>[nodesCount];
+
+            for (int node = 0; node < result.Length; node++)
+            {
+                result[node] = new List<Edge>();
+            }
+
+            for (int i = 0; i < edgesCount; i++)
+            {
+                int[] edgeData = Console.ReadLine().Split().Select(int.Parse).ToArray();
+                int first = edgeData[0];
+                int second = edgeData[1];
+                int weight = edgeData[2];
+                
+                Edge edge = new Edge
+                {
+                    First = first,
+                    Second = second,
+                    Weight = weight
+                };
+
+                result[first].Add(edge);
+                result[second].Add(edge);
+            }
+
+            return result;
+        }
+    }
+
+    public class Edge
+    {
+        public int First { get; set; }
+
+        public int Second { get; set; }
+
+        public int Weight { get; set; }
+    }
+}
